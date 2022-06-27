a2642a72a8f4e7313d96685aa548fd1f08ba898e
Ubuntu <ubuntu@ip-172-31-6-134.ap-northeast-1.compute.internal>
2022-06-27 06:59:35 +0000

    add: install docker-compose

--- mlflow/server-settings.sh
+++ mlflow/server-settings.sh
@@ -19,3 +19,6 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
+# docker comopse
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
